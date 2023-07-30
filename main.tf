79ff83b99d317c55a8d9b1f8863e13552417d5f3
naoki-yazawa <naoki.yazawa@dena.com>
2023-07-30 22:33:57 +0900

    build CI/CD pipeline with GitHub Actions

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  # Assumes s3 bucket and dynamo DB table already set up
+  backend "s3" {
+    bucket         = "bined-tf-state"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "bined-tf-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
