adfd8846e6acb1a2b910bea2fdb89fa130a16f44
naoki-yazawa <naoki.yazawa@dena.com>
2023-07-31 04:47:10 +0900

    remove all

--- rds.tf
+++ rds.tf
@@ -49,20 +49,20 @@
 # }
 
 
-resource "aws_db_instance" "main" {
-  identifier              = local.common_name
-  db_name                 = "hasuradefault"
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = "postgres"
-  engine_version          = "15.2"
-  instance_class          = "db.t3.small"
-  db_subnet_group_name    = module.network.database_subnet_group_name
-  password                = "hasurapassword"
-  username                = "hasurauser"
-  backup_retention_period = 0
-  multi_az                = false
-  skip_final_snapshot     = true
-  deletion_protection     = false
-  vpc_security_group_ids  = [module.security_groups.security_group_rds_id]
-}
+# resource "aws_db_instance" "main" {
+#   identifier              = local.common_name
+#   db_name                 = "hasuradefault"
+#   allocated_storage       = 20
+#   storage_type            = "gp2"
+#   engine                  = "postgres"
+#   engine_version          = "15.2"
+#   instance_class          = "db.t3.small"
+#   db_subnet_group_name    = module.network.database_subnet_group_name
+#   password                = "hasurapassword"
+#   username                = "hasurauser"
+#   backup_retention_period = 0
+#   multi_az                = false
+#   skip_final_snapshot     = true
+#   deletion_protection     = false
+#   vpc_security_group_ids  = [module.security_groups.security_group_rds_id]
+# }
