945adae5e49af0fabbd057516dbd2cc9cdc3ef91
naoki-yazawa <naoki.yazawa@dena.com>
2023-08-01 01:19:26 +0900

    remove all

--- alb.tf
+++ alb.tf
@@ -1,9 +1,9 @@
-module "alb" {
-  source                = "./modules/alb/"
-  vpc_id                = module.network.vpc_id
-  public_subnet_ids     = module.network.public_subnets
-  security_group_alb_id = module.security_groups.security_group_alb_id
-  alb_name              = local.common_name
-  alb_target_group_name = "${local.common_name}-alb-target-group"
-  alb_log_name          = "${local.common_name}-alb-log-bucket"
-}
+# module "alb" {
+#   source                = "./modules/alb/"
+#   vpc_id                = module.network.vpc_id
+#   public_subnet_ids     = module.network.public_subnets
+#   security_group_alb_id = module.security_groups.security_group_alb_id
+#   alb_name              = local.common_name
+#   alb_target_group_name = "${local.common_name}-alb-target-group"
+#   alb_log_name          = "${local.common_name}-alb-log-bucket"
+# }
--- ecs.tf
+++ ecs.tf
@@ -1,15 +1,15 @@
-module "ecs" {
-  source = "./modules/ecs/"
+# module "ecs" {
+#   source = "./modules/ecs/"
 
-  ecs_cluster_name             = local.common_name
-  ecs_service_name             = local.common_name
-  ecs_service_desired_count    = 1
-  private_subnets              = module.network.private_subnets
-  security_group_ecs_id        = module.security_groups.security_group_ecs_id
-  alb_target_group_arn         = module.alb.target_group_arn
-  container_name               = local.common_name
-  container_port               = 8080
-  family                       = local.common_name
-  cloudwatch_log_group_name    = local.common_name
-  ecs_task_execution_role_name = "ecs-task-execution-role-name-dev"
-}
+#   ecs_cluster_name             = local.common_name
+#   ecs_service_name             = local.common_name
+#   ecs_service_desired_count    = 1
+#   private_subnets              = module.network.private_subnets
+#   security_group_ecs_id        = module.security_groups.security_group_ecs_id
+#   alb_target_group_arn         = module.alb.target_group_arn
+#   container_name               = local.common_name
+#   container_port               = 8080
+#   family                       = local.common_name
+#   cloudwatch_log_group_name    = local.common_name
+#   ecs_task_execution_role_name = "ecs-task-execution-role-name-dev"
+# }
