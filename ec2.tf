8950a6d9082899cd854e4aff01583ad9f41c51d6
naoki-yazawa <naoki.yazawa@dena.com>
2023-07-31 09:04:49 +0900

    create all resource

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
-# data "aws_ssm_parameter" "amzn2_ami" {
-#   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-# }
+data "aws_ssm_parameter" "amzn2_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
 
-# module "ec2" {
-#   source = "terraform-aws-modules/ec2-instance/aws"
+module "ec2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-#   name = "bastion-server-dev"
+  name = "bastion-server-dev"
 
-#   ami                         = data.aws_ssm_parameter.amzn2_ami.value
-#   instance_type               = "t2.micro"
-#   availability_zone           = "ap-northeast-1a"
-#   subnet_id                   = element(module.network.public_subnets, 0)
-#   vpc_security_group_ids      = [module.security_groups.security_group_bastion_id]
-#   associate_public_ip_address = true
-# }
+  ami                         = data.aws_ssm_parameter.amzn2_ami.value
+  instance_type               = "t2.micro"
+  availability_zone           = "ap-northeast-1a"
+  subnet_id                   = element(module.network.public_subnets, 0)
+  vpc_security_group_ids      = [module.security_groups.security_group_bastion_id]
+  associate_public_ip_address = true
+}
