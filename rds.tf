48bd6dc528563c83162ed4cf0fbb329f501a6ddc
naoki-yazawa <naoki.yazawa@dena.com>
2023-08-01 00:51:09 +0900

    create all

--- rds.tf
+++ rds.tf
@@ -1,17 +1,25 @@
-# resource "aws_db_instance" "main" {
-#   identifier              = local.common_name
-#   db_name                 = "hasuradefault"
-#   allocated_storage       = 20
-#   storage_type            = "gp2"
-#   engine                  = "postgres"
-#   engine_version          = "15.2"
-#   instance_class          = "db.t3.small"
-#   db_subnet_group_name    = module.network.database_subnet_group_name
-#   password                = "hasurapassword"
-#   username                = "hasurauser"
-#   backup_retention_period = 0
-#   multi_az                = false
-#   skip_final_snapshot     = true
-#   deletion_protection     = false
-#   vpc_security_group_ids  = [module.security_groups.security_group_rds_id]
-# }
+data "aws_secretsmanager_secret" "db_password" {
+  name = "bined/db-password"
+}
+
+data "aws_secretsmanager_secret_version" "db_password" {
+  secret_id = data.aws_secretsmanager_secret.db_password.id
+}
+
+resource "aws_db_instance" "main" {
+  identifier              = local.common_name
+  db_name                 = "hasuradefault"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "15.2"
+  instance_class          = "db.t3.small"
+  db_subnet_group_name    = module.network.database_subnet_group_name
+  password                = data.aws_secretsmanager_secret_version.db_password.secret_string
+  username                = "hasurauser"
+  backup_retention_period = 0
+  multi_az                = false
+  skip_final_snapshot     = true
+  deletion_protection     = false
+  vpc_security_group_ids  = [module.security_groups.security_group_rds_id]
+}
--- security_groups.tf
+++ security_groups.tf
@@ -1,11 +1,11 @@
-# module "security_groups" {
-#   source = "./modules/security_groups"
+module "security_groups" {
+  source = "./modules/security_groups"
 
-#   vpc_id                          = module.network.vpc_id
-#   app_port                        = 8080
-#   database_port                   = 5432
-#   aws_security_group_alb_name     = "alb-sg"
-#   aws_security_group_ecs_name     = "ecs-sg"
-#   aws_security_group_rds_name     = "rds-sg"
-#   aws_security_group_bastion_name = "bastion-sg"
-# }
+  vpc_id                          = module.network.vpc_id
+  app_port                        = 8080
+  database_port                   = 5432
+  aws_security_group_alb_name     = "alb-sg"
+  aws_security_group_ecs_name     = "ecs-sg"
+  aws_security_group_rds_name     = "rds-sg"
+  aws_security_group_bastion_name = "bastion-sg"
+}
